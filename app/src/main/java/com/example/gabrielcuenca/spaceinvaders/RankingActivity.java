10a4e212d7301e7a52061a552dbedac8e1568136
Ana <ani_al_or@hotmail.com>
2018-11-04 17:20:45 +0100

    Ranking
    No funciona pero ya no para el juego

--- app/src/main/java/com/example/gabrielcuenca/spaceinvaders/RankingActivity.java
+++ app/src/main/java/com/example/gabrielcuenca/spaceinvaders/RankingActivity.java
@@ -14,6 +14,7 @@ public class RankingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranking);
 
         Ranking.load();
 
